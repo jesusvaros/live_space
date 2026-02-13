70beb9c29193d4de8732b2c1150c072b1164fab3
jesusvaros <xjesusvr@gmail.com>
2026-02-13 13:24:31 +0100

    remove unused legacy components and test pages, clean up EventCard, EventsList, TimelinePlayer, UploadForm, MapTest, and OrganizerEventsTab in favor of feature-based architecture

--- src/components/EventsList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const EventsList: React.FC = () => {
-  return <div>EventsList component coming soon...</div>;
-};
-
-export default EventsList;
--- src/components/UploadForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UploadForm: React.FC = () => {
-  return <div>UploadForm component coming soon...</div>;
-};
-
-export default UploadForm;
